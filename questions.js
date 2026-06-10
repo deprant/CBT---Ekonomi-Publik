3092dd3e56e1c474fce39630d79c34a5010fe8b5
ade paranata <61595530+deprant@users.noreply.github.com>
2026-06-10 11:23:07 +0700

    Update and rename question.js to questions.js

---
+++ questions.js
@@ -0,0 +1,102 @@
+const questions = [
+  {
+    question: "Apa fungsi utama pajak dalam ekonomi publik?",
+    options: [
+      "Meningkatkan keuntungan perusahaan",
+      "Membiayai pengeluaran pemerintah",
+      "Mengurangi jumlah penduduk",
+      "Meningkatkan impor"
+    ],
+    answer: 1
+  },
+  {
+    question: "Barang publik memiliki karakteristik utama berupa?",
+    options: [
+      "Rival dan eksklusif",
+      "Rival dan non-eksklusif",
+      "Non-rival dan non-eksklusif",
+      "Eksklusif dan mahal"
+    ],
+    answer: 2
+  },
+  {
+    question: "Contoh barang publik adalah?",
+    options: [
+      "Nasi goreng",
+      "Mobil pribadi",
+      "Lampu jalan",
+      "Sepeda motor"
+    ],
+    answer: 2
+  },
+  {
+    question: "Kegagalan pasar terjadi ketika?",
+    options: [
+      "Pasar bekerja sempurna",
+      "Pemerintah tidak memungut pajak",
+      "Alokasi sumber daya tidak efisien",
+      "Semua perusahaan untung"
+    ],
+    answer: 2
+  },
+  {
+    question: "Eksternalitas negatif contohnya adalah?",
+    options: [
+      "Pendidikan",
+      "Vaksinasi",
+      "Polusi pabrik",
+      "Taman kota"
+    ],
+    answer: 2
+  },
+  {
+    question: "Tujuan redistribusi pendapatan adalah?",
+    options: [
+      "Meningkatkan ketimpangan",
+      "Mengurangi kesenjangan ekonomi",
+      "Meningkatkan inflasi",
+      "Mengurangi investasi"
+    ],
+    answer: 1
+  },
+  {
+    question: "APBN merupakan singkatan dari?",
+    options: [
+      "Anggaran Pendapatan dan Belanja Negara",
+      "Anggaran Perdagangan dan Belanja Nasional",
+      "Asosiasi Pengelola Belanja Nasional",
+      "Anggaran Pembangunan Bank Negara"
+    ],
+    answer: 0
+  },
+  {
+    question: "Subsidi diberikan pemerintah dengan tujuan utama?",
+    options: [
+      "Meningkatkan harga barang",
+      "Mengurangi produksi",
+      "Membantu kelompok atau sektor tertentu",
+      "Menghapus pajak"
+    ],
+    answer: 2
+  },
+  {
+    question: "Pajak yang dikenakan atas konsumsi barang dan jasa di Indonesia adalah?",
+    options: [
+      "PPh",
+      "PPN",
+      "PBB",
+      "Bea Meterai"
+    ],
+    answer: 1
+  },
+  {
+    question: "Salah satu peran pemerintah dalam ekonomi publik adalah?",
+    options: [
+      "Menghilangkan seluruh pasar",
+      "Mengatur dan memperbaiki kegagalan pasar",
+      "Menghapus seluruh pajak",
+      "Mengendalikan seluruh harga dunia"
+    ],
+    answer: 1
+  }
+];
